61ad8d653d955e2a697a9679736d38007baf169a
Krishna <krishna444@gmail.com>
2017-11-29 15:08:40 +0100

    Add files via upload

---
+++ src/main/java/com/kpaudel/vaadin/Application.java
@@ -0,0 +1,60 @@
+package com.kpaudel.vaadin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Spring Boot
+ * 
+ * @author Krishna Paudel
+ *
+ *         Nov 29, 2017
+ */
+@SpringBootApplication
+public class Application {
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(CustomerRepository repository) {
+		return (args) -> {
+			// save a couple of customers
+			repository.save(new Customer("Krishna", "Paudel"));
+			repository.save(new Customer("Kabita", "Parajuli"));
+			repository.save(new Customer("Karina", "Kapoor"));
+			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("Michelle", "Dessler"));
+
+			// fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+			log.info("");
+
+			// fetch an individual customer by ID
+			Customer customer = repository.findOne(1L);
+			log.info("Customer found with findOne(1L):");
+			log.info("--------------------------------");
+			log.info(customer.toString());
+			log.info("");
+
+			// fetch customers by last name
+			log.info("Customer found with findByLastNameStartsWithIgnoreCase('Paudel'):");
+			log.info("--------------------------------------------");
+			for (Customer bauer : repository.findByLastNameStartsWithIgnoreCase("Kapoor")) {
+				log.info(bauer.toString());
+			}
+			log.info("");
+		};
+	}
+
+}
